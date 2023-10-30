a1dafeb220873f05be9ac05c7b0e138d3e6c7157
Aisalkyn <aisalkynaidarova8@gmail.com>
2023-10-29 20:15:37 -0500

    added

--- Test.java
+++ Test.java
@@ -4,7 +4,8 @@ public class  Test{
 
 public static void main(String [] args){
     System.out.println("Hi");
-    System.out.println("How are you man");
+    System.out.println("How are you man1");
+    
 
 }
 }
